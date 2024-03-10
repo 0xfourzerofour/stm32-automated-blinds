b1404d8d12433c11dbb63d8196460414cde3c120
Joshua Pauline <joshpauline@gmail.com>
2024-03-10 16:58:43 -0400

    feat(blinds): add direction logic

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@
 
 use panic_rtt_target as _;
 use rtic::app;
-use rtt_target::{rprintln, rtt_init_print};
+use rtt_target::rtt_init_print;
 use stm32f4xx_hal::gpio::{Edge, Input, PA0};
 use stm32f4xx_hal::prelude::*;
+use stm32f4xx_hal::timer::{Channel1, Channel2};
 
 #[app(device = stm32f4xx_hal::pac, peripherals = true)]
 mod app {
@@ -34,6 +35,8 @@ mod app {
         rtt_init_print!();
         let mut dp = cx.device;
         let mut syscfg = dp.SYSCFG.constrain();
+        let rcc = dp.RCC.constrain();
+        let clocks = rcc.cfgr.freeze();
 
         let gpioa = dp.GPIOA.split();
         let mut receiver = gpioa.pa0.into_pull_up_input();
@@ -41,6 +44,14 @@ mod app {
         receiver.make_interrupt_source(&mut syscfg);
         receiver.trigger_on_edge(&mut dp.EXTI, Edge::Falling);
         receiver.enable_interrupt(&mut dp.EXTI);
+
+        let channels = (Channel1::new(gpioa.pa8), Channel2::new(gpioa.pa9));
+
+        let pwm = dp.TIM1.pwm_hz(channels, 36.kHz(), &clocks).split();
+        let (mut ch1, _ch2) = pwm;
+        let max_duty = ch1.get_max_duty();
+        ch1.set_duty(max_duty / 2);
+        ch1.enable();
 
         (
             Shared {},
@@ -54,10 +65,13 @@ mod app {
     #[task(binds = EXTI0, local = [receiver, direction])]
     fn receive_signal(ctx: receive_signal::Context) {
         ctx.local.receiver.clear_interrupt_pending_bit();
-        rprintln!("{:?}", ctx.local.direction);
         match ctx.local.direction {
-            Direction::Up => *ctx.local.direction = Direction::Down,
-            Direction::Down => *ctx.local.direction = Direction::Up,
+            Direction::Up => {
+                *ctx.local.direction = Direction::Down;
+            }
+            Direction::Down => {
+                *ctx.local.direction = Direction::Up;
+            }
         }
     }
 }
